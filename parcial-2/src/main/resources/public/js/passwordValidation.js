27e13394c4241f9ff3b9ee5cc7b5cd4537c1213f
Dreamy <20170502@ce.pucmm.edu.do>
2021-07-06 10:04:11 -0400

    dashboard graph and url shortener

--- parcial-2/src/main/resources/public/js/passwordValidation.js
+++ parcial-2/src/main/resources/public/js/passwordValidation.js
@@ -1,17 +1,20 @@
 
 let form = document.getElementById("form");
-form.addEventListener('submit', (ev)=>{
-   ev.preventDefault();
-   areEqual(ev);
-});
+let myEvent = function (event){
+    ev.preventDefault();
+    areEqual(ev, form);
+}
+form.addEventListener('submit', myEvent);
 
-function areEqual(e){
+function areEqual(e, form){
 
     let password = document.getElementById("password").value;
     let passwordCheck = document.getElementById("passwordCheck").value;
+    
 
     if(passwordCheck.normalize().trim() === password.normalize().trim()){
-        e.unbind('submit').submit();
+        form.removeEventlistener('submit', myEvent);
+        form.submit;
     }else{
         alert("Password are not the same");
     }
