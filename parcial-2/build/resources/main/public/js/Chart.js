16f730e3e722c9e8860a0bf04aad051891fa4cf5
Dreamy <20170502@ce.pucmm.edu.do>
2021-07-07 16:22:56 -0400

    dashboard graph works

--- parcial-2/build/resources/main/public/js/Chart.js
+++ parcial-2/build/resources/main/public/js/Chart.js
@@ -1,14 +1,23 @@
 google.charts.load('current', {packages: ['corechart', 'bar']});
 google.charts.setOnLoadCallback(drawBasic);
 
+let info = JSON.parse(document.getElementById('chart_div').getAttribute('data-dates'));
+console.log(info);
+for(let i=0; i < info.length; i++){
+    info[i].date = info[i].date.year+'-'+info[i].date.month+'-'+info[i].date.day;
+}
+console.log(info);
 function drawBasic() {
 
-    var data = new google.visualization.arrayToDataTable([
-        ['Date', 'Total Clicks'],
-        ['Past yesterday', 48],
-        ['Yesterday', 30],
-        ['Today', 10]
-    ]);
+    let arrayData=[];
+    for(let i=0; i < info.length+1; i ++){
+        if(i === 0){
+            arrayData[i] = ['Date', 'Total Clicks']
+        }else
+            arrayData[i] = [info[i-1].date,info[i-1].quantity]
+    }
+
+    var data = new google.visualization.arrayToDataTable(arrayData);
     var options = {
         hAxis: {
             title: 'Day',
