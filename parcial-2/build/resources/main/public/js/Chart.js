a9ef2ba6026f3aef8d1f8c1304534398cb9f1e02
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2021-08-15 14:08:35 -0400

    Preparacion para SOAP

---
+++ parcial-2/build/resources/main/public/js/Chart.js
@@ -0,0 +1,39 @@
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBasic);
+
+let info = JSON.parse(document.getElementById('chart_div').getAttribute('data-dates'));
+
+for(let i=0; i < info.length; i++){
+    info[i].date = info[i].date.year+'-'+info[i].date.month+'-'+info[i].date.day;
+}
+
+function drawBasic() {
+
+    let arrayData=[];
+    if(info.length > 0){
+        for(let i=0; i < info.length+1; i ++){
+            if(i === 0){
+                arrayData[i] = ['Date', 'Total Clicks']
+            }else
+                arrayData[i] = [info[i-1].date,info[i-1].quantity]
+        }
+
+        var data = new google.visualization.arrayToDataTable(arrayData);
+        var options = {
+            hAxis: {
+                title: 'Day',
+            },
+            vAxis: {
+                title: 'Total Clicks'
+            }
+        };
+
+        var chart = new google.visualization.ColumnChart(
+            document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+    }
+
+}
+
+window.onresize = drawBasic;
