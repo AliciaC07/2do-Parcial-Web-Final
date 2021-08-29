ad3fe06ec2a6a4f3ae81bb441c9c32c2b45bd0bf
Aliciac07 <20170465@ce.pucmm.edu.do>
2021-08-29 00:24:10 -0400

    Arreglo de servicios.

--- parcial-2/src/main/java/org/parcial/controllers/ShortenerController.java
+++ parcial-2/src/main/java/org/parcial/controllers/ShortenerController.java
@@ -22,7 +22,7 @@ public class ShortenerController {
     private UrlService urlService = UrlService.getInstance();
     private VisitService visitService = VisitService.getInstance();
     private CookieVerificationService cookieVerificationService = CookieVerificationService.getInstance();
-    private String domain = "localhost:7001/";
+    private String domain = "shortly.traki-tech.games/";
 
     public ShortenerController(Javalin app) {
         this.app = app;
